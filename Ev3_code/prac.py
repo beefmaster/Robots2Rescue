f87d086b7f5374f68d4dbd921b279fa2da1305af
Cameron Baker <cameronbaker@Camerons-MacBook-Air.local>
2019-03-14 15:29:15 +1100

    first commit

---
+++ Ev3_code/prac.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from ev3dev.ev3 import *
+
+
+import os
+
+
+
+btn = Button() #button stop
+
+Leds.set_colour(Leds.RIGHT, Leds.ORANGE)
+# def main():
+#     mB = LargeMotor('outB')
+#     mC = LargeMotor('outC')
+
+#     while not btn.any():
+#      mB.run_forever(speed_sp=450)
+#      mB.run_forever(speed_sp=450)
+
+
+
+        
