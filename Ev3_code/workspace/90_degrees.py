d62fec3a87fe466cb76da3facbbc477b0661812a
Cameron Baker <cameronbaker@Camerons-MacBook-Air.local>
2019-03-28 15:15:11 +1100

    moved stuff around

---
+++ Ev3_code/workspace/90_degrees.py
@@ -0,0 +1,16 @@
+# program to turn robot exactly 90 degrees
+
+#!/usr/bin/env python3
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, SpeedPercent, MoveTank
+from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.led import Leds
+
+ts = TouchSensor(INPUT_1)
+steering_drive = MoveSteering(OUTPUT_A, OUTPUT_D)
+
+while True:
+    if ts.is_pressed:
+        steering_drive.on_for_rotations(-50, SpeedPercent(50), 2/3)
+    else:
+        steering_drive.on_for_rotations(0, SpeedPercent(0), 0)
