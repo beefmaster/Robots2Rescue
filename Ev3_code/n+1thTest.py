c8857b51b49f70a87b0bd9d1010d572110516eac
TedxRenegade <48080271+TedxRenegade@users.noreply.github.com>
2019-04-08 17:52:04 +1000

    Update n+1thTest.py

--- Ev3_code/n+1thTest.py
+++ Ev3_code/n+1thTest.py
@@ -13,21 +13,6 @@ ultrasonic_sensor_side.mode = 'US-DIST-CM'
 gyro = GyroSensor(INPUT_1)
 gyro.mode = GyroSensor.MODE_GYRO_ANG
 
-drivetrain = MoveSteering(OUTPUT_B, OUTPUT_D)
-
-drivetrain.on(steering = 0, speed = 20)
-from ev3dev.ev3 import *
-from ev3dev2.motor import OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, MediumMotor, MoveSteering, SpeedPercent
-from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
-from ev3dev2.sensor.lego import ColorSensor, GyroSensor, UltrasonicSensor
-
-medMotor = MediumMotor(OUTPUT_C)
-ultrasonic_sensor_front = UltrasonicSensor(INPUT_4)
-ultrasonic_sensor_side = UltrasonicSensor(INPUT_2)
-ultrasonic_sensor_front.mode = 'US-DIST-CM'
-ultrasonic_sensor_side.mode = 'US-DIST-CM'
-gyro = GyroSensor(INPUT_1)
-gyro.mode = 'MODE_GYRO_ANG'
 
 drivetrain = MoveSteering(OUTPUT_B, OUTPUT_D)
 
