a3cd684625aadcaf7fbefdc8d2d6744eb263863e
TedxRenegade <48080271+TedxRenegade@users.noreply.github.com>
2019-04-15 15:58:45 +1000

    Update n+2ndTest.py

--- Ev3_code/n+2ndTest.py
+++ Ev3_code/n+2ndTest.py
@@ -16,8 +16,8 @@ gyro.mode = GyroSensor.MODE_GYRO_ANG
 
 drivetrain = MoveSteering(OUTPUT_B, OUTPUT_D)
 
-front_dist = ultrasonic_sensor_front.distance_centimeters()
-side_dist = ultrasonic_sensor_side.distance_centimeters()
+front_dist = ultrasonic_sensor_front.distance_centimeters.continuous
+side_dist = ultrasonic_sensor_side.distance_centimeters.continuous
 
 while True:
     if front_dist <= 10:
