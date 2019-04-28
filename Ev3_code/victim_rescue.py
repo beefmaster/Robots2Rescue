9eadd390afe2bbb71001d13fb33f4fb30484415d
TedxRenegade <48080271+TedxRenegade@users.noreply.github.com>
2019-04-28 22:59:21 +1000

    Update victim_rescue.py
    the "double check" section only works if the lifted water bottle blocks the front ultrasonic sensor, which is probably so far the only way of telling whether a victim has been picked up; otherwise it would probably lose existing victim.

--- Ev3_code/victim_rescue.py
+++ Ev3_code/victim_rescue.py
@@ -24,6 +24,8 @@ us_side.mode = 'US-DIST-CM'
 colors = ('red', 'blue')
 
 while True:
+    tank_drive.on(SpeedPercent(50), SpeedPercent(0))
+    # if the robot keeps spinning and won't stop, set to rotate for a few seconds.
     if cl.color == 5 or cl.color == 2:
             tank_drive.on_for_seconds(SpeedPercent(0), SpeedPercent(0), 2)
 
@@ -39,11 +41,54 @@ while True:
             # move robot into position
             tank_drive.on_for_seconds(SpeedPercent(-20), SpeedPercent(-20), 2)
             tank_drive.on_for_rotations(SpeedPercent(20), SpeedPercent(-20), 1/5)
+            tank_drive.on_for_seconds(SpeedPercent(20), SpeedPercent(20), 3.5)
+            # time of robot driving forward (3.5) needs to be tested to finalize. 
+       
 
             # lift up scoop
             medMotor.polarity = 'normal'
             medMotor.on_for_rotations(SpeedPercent(50), 2)
+            
+            # drive back, so that if rescue fails it can start over at original position
+            tanl_drive.on_for_seconds(SpeedPercent(20), SpeedPercent(20), 3.5)
         
         elif cl.color == 2:
             leds.set_color("LEFT", "RED")
             leds.set_color("RIGHT", "RED")
+            
+    
+    # the section below only works if the lifted water bottle blocks the front ultrasonic sensor, which is probably so far the 
+    # only way of telling whether a victim has been picked up
+    
+    if us_front.distance_centimeters > 5:
+   
+        tank_drive.on_for_secods(SpeedPercent(50), SpeedPercent(0), 10)
+        #only checking for 10 secs
+        if cl.color == 5 or cl.color == 2:
+            tank_drive.on_for_seconds(SpeedPercent(0), SpeedPercent(0), 2)
+
+            if cl.color == 5    
+                # change LED colours to green if a true victim is detected
+                leds.set_color("LEFT", "GREEN")
+                leds.set_color("RIGHT", "GREEN")
+
+                # lower scoop 
+                medMotor.polarity = 'inversed'
+                medMotor.on_for_rotations(SpeedPercent(50), 2)
+
+                # move robot into position
+                tank_drive.on_for_seconds(SpeedPercent(-20), SpeedPercent(-20), 2)
+                tank_drive.on_for_rotations(SpeedPercent(20), SpeedPercent(-20), 1/5)
+                tank_drive.on_for_seconds(SpeedPercent(20), SpeedPercent(20), 3.5)
+                # time of robot driving forward needs to be tested to finalize. 
+
+                # lift up scoop
+                medMotor.polarity = 'normal'
+                medMotor.on_for_rotations(SpeedPercent(50), 2)
+            
+                # drive back, so that if rescue fails it can start over at original position
+                tanl_drive.on_for_seconds(SpeedPercent(20), SpeedPercent(20), 3.5)
+        
+            elif cl.color == 2:
+                leds.set_color("LEFT", "RED")
+                leds.set_color("RIGHT", "RED")
